55ddd82910a3de3f2c2a69b5527d8336d742b260
Ansh <anshkevadiya1802@gmail.com>
2023-09-12 16:40:56 +0530

    if Company name matches then only it will allow message to go to api

--- app/src/main/java/com/example/otpfinal/OTP_Receiver.java
+++ app/src/main/java/com/example/otpfinal/OTP_Receiver.java
@@ -1,10 +1,14 @@
 package com.example.otpfinal;
 
+import android.annotation.TargetApi;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
 import android.provider.Telephony;
 import android.telephony.SmsMessage;
+import android.util.Log;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -12,11 +16,13 @@ import android.widget.Toast;
 public class OTP_Receiver extends BroadcastReceiver
 {
     private static EditText editText1;
+    @TargetApi(Build.VERSION_CODES.M)
 
     public void setEditText(EditText editText)
     {
         OTP_Receiver.editText1 = editText;
     }
+
 
     @Override
     public void onReceive(Context context, Intent intent)
@@ -28,18 +34,28 @@ public class OTP_Receiver extends BroadcastReceiver
         for (SmsMessage sms : messages)
         {
             String message = sms.getMessageBody();
-            if (message.contains("From")||message.contains("To")||message.contains("Ansh"))
+            String smsmessage=sms.getOriginatingAddress();
+
+
+            if (smsmessage.contains("Jio")||smsmessage.contains("ICICI")||smsmessage.contains("6505551212"))
             {
-                editText1.setText(message);
+                editText1.setText(smsmessage);
             }
 
 
 
         }
+
+
+
+
+    }
+
+
+
     }
 
 
 
 
 
-}
